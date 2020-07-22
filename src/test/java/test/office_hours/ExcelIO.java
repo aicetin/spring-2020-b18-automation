65fa917622db2d8a194b27cec4d4871612df6b5b
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-07-22 15:27:16 -0400

    ExcelIO practice

--- src/test/java/test/office_hours/ExcelIO.java
+++ src/test/java/test/office_hours/ExcelIO.java
@@ -8,8 +8,8 @@ import java.io.FileInputStream;
 public class ExcelIO {
 
     @Test
-    public void readExcelFile(){
-        try(FileInputStream fileInputStream = new FileInputStream("VytrackTestUsers.xlsx")){
+    public void readExcelFile() {
+        try (FileInputStream fileInputStream = new FileInputStream("VytrackTestUsers.xlsx")) {
             //.xlsx
             //XSSFWorkbook - to create an object of .xlsx  file
             //HSSFWorkbook - to create an object of .xls  file
@@ -25,7 +25,34 @@ public class ExcelIO {
             Cell cell = row.getCell(0);
             //read a value from 1st row 1st column
             System.out.println(cell);
-        }catch (Exception e){
+
+            //print every cell in the first row
+            int columnNumber = row.getPhysicalNumberOfCells();//how many cells
+            for (int i = 0; i < columnNumber; i++) {
+                System.out.print(row.getCell(i) + " ");
+            }
+            System.out.println();
+            //to get number of rows
+            int rowNumber = sheet.getPhysicalNumberOfRows();
+            //print first and seconds columns
+            for (int i = 0; i < rowNumber; i++) {
+                System.out.print(sheet.getRow(i).getCell(0) + "    ");
+                System.out.print(sheet.getRow(i).getCell(1) + "    ");
+                System.out.print(sheet.getRow(i).getCell(2) + "     ");
+                System.out.print(sheet.getRow(i).getCell(3));
+                System.out.println();
+            }
+            System.out.println("###################################################");
+            //to print entire table
+            for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++) {
+                for (int columnIndex = 0; columnIndex < columnNumber; columnIndex++) {
+                    System.out.print(sheet.getRow(rowIndex).getCell(columnIndex) + " ");
+                }
+                System.out.println();
+            }
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
