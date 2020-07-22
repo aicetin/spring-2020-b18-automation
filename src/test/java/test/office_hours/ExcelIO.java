b919351e1163a3343762644f9b71778f6ddc3529
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-07-22 15:51:13 -0400

    write data to excel file

--- src/test/java/test/office_hours/ExcelIO.java
+++ src/test/java/test/office_hours/ExcelIO.java
@@ -4,6 +4,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Arrays;
 
 public class ExcelIO {
@@ -45,13 +46,16 @@ public class ExcelIO {
             }
             System.out.println("###################################################");
             //to print entire table
-
+            //here we create 2D array to store data from excel file
+            //to specify size of array we use number of rows and columns
             String[][] dataTable = new String[rowNumber][columnNumber];
 
             for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++) {
                 for (int columnIndex = 0; columnIndex < columnNumber; columnIndex++) {
                     String value = sheet.getRow(rowIndex).getCell(columnIndex).getStringCellValue();
+                    //to print value exactly 15 characters long
                     System.out.format("%15s", value);
+                    //we write data into 2D array
                     dataTable[rowIndex][columnIndex] = value;
                 }
                 System.out.println();
@@ -60,6 +64,28 @@ public class ExcelIO {
             System.out.println(Arrays.deepToString(dataTable));
 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void writeIntoExcelFile(){
+        //input stream - read
+        //output stream - write
+        //NOTE: close excel file during writing. Otherwise, file will crash.
+        try(FileInputStream fileInputStream = new FileInputStream("VytrackTestUsers.xlsx")){
+            Workbook workbook = WorkbookFactory.create(fileInputStream);
+            Sheet sheet = workbook.getSheet("QA1-short");
+            Row row = sheet.getRow(1); //get second row
+            //write PASSED to 2nd row 6th column
+            row.getCell(5).setCellValue("PASSED");
+
+
+            FileOutputStream fileOutputStream = new FileOutputStream("VytrackTestUsers.xlsx");
+            workbook.write(fileOutputStream); //to write updates to the file
+            fileOutputStream.close();
+
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
